4668848ced5afc3139899ddc939f38a74f864d05
Eva Decker <itsevadecker@gmail.com>
2023-10-19 16:58:18 -0400

    Enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://genderswap.fm",
   integrations: [sitemap()],
+  output: "server",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
