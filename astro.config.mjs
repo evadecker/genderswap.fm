24b8bf875303fde096af72ef52941d3603fd2c2a
Eva Decker <itsevadecker@gmail.com>
2023-10-27 12:56:07 -0400

    feat: Enable Vercel speed insights (#57)

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,10 @@ export default defineConfig({
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true
-    }
-  })
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+  }),
 });
