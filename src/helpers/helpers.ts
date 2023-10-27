d58138cbdd1cb851fe4180ad8276cc851668a6a0
Eva Decker <itsevadecker@gmail.com>
2023-10-27 16:09:13 -0400

    Use smart quotes

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -59,3 +59,13 @@ export const slugifyCover = (name: string, artist: string) => {
   const slug = `${slugify(removeSongExtraText(name))}-${slugify(artist)}`;
   return slug;
 };
+
+export const smartquotes = (str: string) => {
+  return str
+    .replace(/(^|[-\u2014\s(\["])'/g, "$1\u2018") // opening singles
+    .replace(/'/g, "\u2019") // closing singles & apostrophes
+    .replace(/(^|[-\u2014/\[(\u2018\s])"/g, "$1\u201c") // opening doubles
+    .replace(/"/g, "\u201d") // closing doubles
+    .replace(/--/g, "\u2014") // em-dashes
+    .replace(/\.\.\./g, "\u2026"); // ellipses
+};
