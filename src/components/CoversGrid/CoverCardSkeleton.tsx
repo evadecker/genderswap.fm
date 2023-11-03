87bb5fd782561f42c3d492b478d1f5d363578bc7
Eva Decker <itsevadecker@gmail.com>
2023-11-03 15:16:44 -0400

    Set static width for skeleton text

--- src/components/CoversGrid/CoverCardSkeleton.tsx
+++ src/components/CoversGrid/CoverCardSkeleton.tsx
@@ -1,26 +1,15 @@
 import classnames from "classnames";
 import styles from "./coversGrid.module.scss";
 
-export const CoverCardSkeleton = () => {
-  const getRandomWidth = () => {
-    const maxWidth = 90;
-    const minWidth = 40;
-    return Math.random() * (maxWidth - minWidth) + minWidth + "%";
-  };
-
-  return (
-    <div
-      className={classnames(styles.coverCard, styles.placeholder)}
-      aria-hidden
-    >
-      <div className={styles.albums}>
-        <div className={styles.album}></div>
-        <div className={styles.album}></div>
-      </div>
-      <div className={styles.content}>
-        <h2 className={styles.title} style={{ width: getRandomWidth() }} />
-        <div className={styles.artist} style={{ width: getRandomWidth() }} />
-      </div>
+export const CoverCardSkeleton = () => (
+  <div className={classnames(styles.coverCard, styles.placeholder)} aria-hidden>
+    <div className={styles.albums}>
+      <div className={styles.album}></div>
+      <div className={styles.album}></div>
     </div>
-  );
-};
+    <div className={styles.content}>
+      <h2 className={styles.title} />
+      <div className={styles.artist} />
+    </div>
+  </div>
+);
