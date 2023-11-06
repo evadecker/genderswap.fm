694ec794091abbac1102b5779979313a249f25cc
Eva Decker <itsevadecker@gmail.com>
2023-11-05 20:42:16 -0500

    Clean Sveltekit install

--- src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import type { Database } from "../types/db-generated.types";
-
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
