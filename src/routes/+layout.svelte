64c9c91381d7e7026ddc06374ab0be0b4f2656be
Eva Decker <itsevadecker@gmail.com>
2023-11-22 01:32:29 -0500

    fix: Enable Vercel Analytics (#131)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,12 @@
   import '$lib/styles/theme.css';
   import '$lib/styles/base.css';
 
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
   import Header from '$lib/components/Header.svelte';
   import Footer from '$lib/components/Footer.svelte';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Header />
