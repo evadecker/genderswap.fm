5b8e067c19208663f1a6348ce7c82e04e6de1fbb
Eva Decker <itsevadecker@gmail.com>
2023-11-23 16:00:34 -0500

    Add confetti on new submission

--- src/routes/cover/[slug]/+page.svelte
+++ src/routes/cover/[slug]/+page.svelte
@@ -5,10 +5,64 @@
   import Tag from '$lib/components/Tag.svelte';
   import { TAGS } from '$lib/constants';
   import { page } from '$app/stores';
+  import { onMount } from 'svelte';
+  import confetti from 'canvas-confetti';
 
   export let data;
+  const isNew = $page.url.searchParams.get('new') === 'true' ? true : false;
 
   const formattedDate = dayjs(data.created_at).format('MMMM D, YYYY');
+
+  onMount(async () => {
+    const fireConfetti = (placement: 'left' | 'right') => {
+      // https://www.flagcolorcodes.com/transgender
+      const colors = ['#ffffff', '#5BCEFA', '#F5A9B8'];
+      const baseAngle = placement === 'left' ? 70 : 110;
+      const origin = placement === 'left' ? { x: -0, y: 1 } : { x: 1, y: 1 };
+      const drift = placement === 'left' ? 0.5 : -0.5;
+
+      const sharedProps = {
+        particleCount: 100,
+        scalar: 0.8,
+        angle: baseAngle,
+        ticks: 300,
+        origin,
+        drift,
+        colors,
+        disableForReducedMotion: true,
+        zIndex: 999
+      };
+
+      // The strongest blast of confetti, centered on the baseAngle
+      confetti({
+        ...sharedProps,
+        spread: 20,
+        startVelocity: 90,
+        decay: 0.92
+      });
+
+      // A weaker last of confetti with a larger spread
+      confetti({
+        ...sharedProps,
+        spread: 50,
+        startVelocity: 60,
+        decay: 0.9
+      });
+
+      // The weakest blast of confetti
+      confetti({
+        ...sharedProps,
+        spread: 60,
+        startVelocity: 40,
+        decay: 0.89
+      });
+    };
+
+    if (isNew) {
+      setTimeout(() => fireConfetti('left'), 600);
+      setTimeout(() => fireConfetti('right'), 1200);
+    }
+  });
 </script>
 
 <svelte:head>
