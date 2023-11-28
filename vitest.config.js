311b0f915865858154765dac49357b6f99a5accb
Eva Decker <itsevadecker@gmail.com>
2023-11-28 17:26:40 -0500

    Exclude Playwright tests from Vitest

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
+    include: ['**/*.test.?(c|m)[jt]s?(x)'],
     coverage: {
       reporter: ['text', 'json-summary', 'json'],
       lines: 100,
