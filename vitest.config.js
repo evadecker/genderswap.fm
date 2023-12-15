9863e6a327e258518ed6ce6548fda99832a3fd98
Eva Decker <itsevadecker@gmail.com>
2023-12-14 19:10:15 -0500

    Remove coverage minimums

--- vitest.config.js
+++ vitest.config.js
@@ -4,11 +4,7 @@ export default defineConfig({
   test: {
     include: ['**/*.test.?(c|m)[jt]s?(x)'],
     coverage: {
-      reporter: ['text', 'json-summary', 'json'],
-      lines: 100,
-      branches: 100,
-      functions: 100,
-      statements: 100
+      reporter: ['text', 'json-summary', 'json']
     }
   }
 });
