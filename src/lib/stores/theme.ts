4b14d4516d973d8c99d606081351f864084dac04
Eva Decker <itsevadecker@gmail.com>
2025-07-09 22:34:11 -0400

    fix: Update meta theme color correctly (#351)

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -3,12 +3,41 @@ import { writable } from 'svelte/store';
 
 type Theme = 'light' | 'dark';
 
+const updateThemeColor = () => {
+  if (!browser) return;
+  const bgColor = window.getComputedStyle(document.documentElement).backgroundColor;
+  const metaThemeColor = document.querySelector("meta[name=theme-color]");
+  metaThemeColor?.setAttribute("content", bgColor);
+};
+
+const updateThemeToggle = (theme: Theme) => {
+  if (!browser) return;
+  const lightToggle = document.querySelector('[data-theme-toggle-light]');
+  const darkToggle = document.querySelector('[data-theme-toggle-dark]');
+
+  if (theme === 'light') {
+    lightToggle?.classList.add('active');
+    darkToggle?.classList.remove('active');
+  } else {
+    lightToggle?.classList.remove('active');
+    darkToggle?.classList.add('active');
+  }
+};
+
+const getUserPreference = (): Theme => {
+  if (!browser) return 'light';
+  
+  if (window.localStorage.getItem('theme')) {
+    return window.localStorage.getItem('theme') as Theme;
+  }
+
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+};
+
 let initialTheme: Theme = 'light';
 
 if (browser) {
-  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
-  const defaultValue: Theme = prefersDarkMode.matches ? 'dark' : 'light';
-  initialTheme = (window.localStorage.getItem('theme') as Theme) ?? defaultValue;
+  initialTheme = getUserPreference();
 }
 
 export const theme = writable<Theme>(initialTheme);
@@ -16,6 +45,9 @@ export const theme = writable<Theme>(initialTheme);
 theme.subscribe((value) => {
   if (browser) {
     localStorage.setItem('theme', value);
-    document.body.classList.toggle('dark', value === 'dark');
+    document.documentElement.classList.toggle('dark', value === 'dark');
+    
+    updateThemeColor();
+    updateThemeToggle(value);
   }
 });
